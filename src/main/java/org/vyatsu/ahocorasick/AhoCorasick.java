5ddbbcae654c4facda8d167f8fc4b8285a9bcbb3
fitumi0 <chervyakov2002anton@mail.ru>
2025-02-15 11:37:20 +0300

    first commit

---
+++ src/main/java/org/vyatsu/ahocorasick/AhoCorasick.java
@@ -0,0 +1,12 @@
+package org.vyatsu.ahocorasick;
+
+/**
+ *
+ * @author fitumi0
+ */
+public class AhoCorasick {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
