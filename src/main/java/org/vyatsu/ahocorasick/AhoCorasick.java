9064ebe6c011100fd63774f7ac31d33f43d5f190
fitumi0 <chervyakov2002anton@mail.ru>
2025-03-05 23:13:43 +0300

    implemented: search for patterns

--- src/main/java/org/vyatsu/ahocorasick/AhoCorasick.java
+++ src/main/java/org/vyatsu/ahocorasick/AhoCorasick.java
@@ -1,9 +1,12 @@
 package org.vyatsu.ahocorasick;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+
+import org.vyatsu.ahocorasick.utils.Pair;
 
 /**
  *
@@ -61,7 +64,44 @@ public class AhoCorasick {
         }
     }
 
-    public Map<String, List<Integer>> search(String text) {
-        return null;
+    public List<Pair> search(String text) {
+        List<Pair> result = new ArrayList<>();
+    
+        TrieNode current = this.root;
+        int i = 0;
+    
+        while (i < text.length()) {
+            char c = text.charAt(i);
+    
+            // Follow suffix links if current node not root and not have the char
+            while (current != this.root && !current.hasChild(c)) {
+                current = current.getSuffixLink();
+            }
+    
+            // If child exists, assign new current
+            if (current.hasChild(c)) {
+                current = current.getChild(c);
+                // And go to next char
+                i++; 
+    
+                // Check for matches in the current node and its suffix links
+                TrieNode temp = current;
+                while (temp != this.root) {
+                    for (String output : temp.getOutputs()) {
+                        result.add(new Pair(i - output.length(), output));
+                    }
+
+                    // Following a non-root suffix link probably we can find an complete pattern
+                    // e.g. for patterns "he", "she" in text "she" will be "she" at 0 pos and "he" at 1 pos
+                    temp = temp.getSuffixLink();
+                }
+            } else {
+                // If no match and current is root, move to the next character
+                i++;
+            }
+        }
+    
+        return result;
     }
+    
 }
